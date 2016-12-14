89ce0db962f0a09f59a3316220d28ff144461ceb
Tobias <tbr1025@students.fhv.at>
2016-12-14 15:31:35 +0100

    Beispiel integriert zum generieren

---
+++ Code/Generated/src/at/fhv/TimeTracker/Main.java
@@ -0,0 +1,21 @@
+package at.fhv.TimeTracker;
+public class Main {
+    /**
+     * Description of the property test.
+     */
+    public String test = "";
+    
+    // Start of user code (user defined attributes)
+    
+    // End of user code
+    
+		public String getTest(){
+		   return this.test;
+		}
+		
+		public void setTest(String test){
+		   this.test = test;
+		}
+		
+	
+}
