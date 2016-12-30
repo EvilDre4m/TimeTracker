ab4999889cd5bd8276a88618144b2145e21d49b5
Aripas <tobias.breuss@students.fhv.at>
2016-12-30 10:54:51 +0100

    Merge mit plain java

---
+++ TimeTracker_PlainJavaImplementation/PJI2/src/at/fhv/timetracker/project/ProjectService.java
@@ -0,0 +1,21 @@
+package at.fhv.timetracker.project;
+
+public class ProjectService {
+
+	public void addProject(){
+		//TODO Signature and body
+	}
+	
+	public void searchProject(){
+		//TODO Signature and body
+	}
+	
+	public void editProject(){
+		//TODO Signature and body
+	}
+	
+	public void deleteProject(){
+		//TODO Signature and body
+	}
+	
+}
